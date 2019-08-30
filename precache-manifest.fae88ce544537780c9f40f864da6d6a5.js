4f005de988ea7d68ebd147da5cc5dae1a097e0b0
voslikovskyi <voslikovskyi@n-ix.com>
2019-08-30 11:09:58 +0300

    Updates

---
+++ precache-manifest.fae88ce544537780c9f40f864da6d6a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab71385319014a488e51a8e79cf10574",
+    "url": "/list/index.html"
+  },
+  {
+    "revision": "6f8de0108dfb2a13df20",
+    "url": "/list/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "80deedeba68693bc35f8",
+    "url": "/list/static/js/2.b0d693f1.chunk.js"
+  },
+  {
+    "revision": "6f8de0108dfb2a13df20",
+    "url": "/list/static/js/main.14b67bc8.chunk.js"
+  },
+  {
+    "revision": "7c9128c9ce3e78a63e05",
+    "url": "/list/static/js/runtime~main.f72228e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/list/static/media/logo.5d5d9eef.svg"
+  }
+]);
